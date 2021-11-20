99d4c103794d10cf45c0042ed482d1eb2e1730c9
Harnick Khera <20884880+Hephyrius@users.noreply.github.com>
2021-11-20 14:30:43 +0000

    Update ChatsList.js

--- Dapp/src/Components/ChatsList.js
+++ Dapp/src/Components/ChatsList.js
@@ -21,49 +21,10 @@ export default function ChatsList() {
       <List className={classes.cardWithBGList}>
         <ChatsListItem />
         <Divider variant="inset" component="li" />
-        <ListItem button  alignItems="flex-start">
-          <ListItemAvatar>
-            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-          </ListItemAvatar>
-          <ListItemText
-            primary="Summer BBQ"
-            secondary={
-              <React.Fragment>
-                <Typography
-                  component="span"
-                  variant="body2"
-                  className={classes.inline}
-                  color="textPrimary"
-                >
-                  to Scott, Alex, Jennifer
-                </Typography>
-                {" — Wish I could come, but I'm out of town this…"}
-              </React.Fragment>
-            }
-          />
-        </ListItem>
+        <ChatsListItem />
         <Divider variant="inset" component="li" />
-        <ListItem button  alignItems="flex-start">
-          <ListItemAvatar>
-            <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-          </ListItemAvatar>
-          <ListItemText
-            primary="Oui Oui"
-            secondary={
-              <React.Fragment>
-                <Typography
-                  component="span"
-                  variant="body2"
-                  className={classes.inline}
-                  color="textPrimary"
-                >
-                  Sandra Adams
-                </Typography>
-                {' — Do you have Paris recommendations? Have you ever…'}
-              </React.Fragment>
-            }
-          />
-        </ListItem>
+        <ChatsListItem />
+        <Divider variant="inset" component="li" />
       </List>
     </div>
   );
