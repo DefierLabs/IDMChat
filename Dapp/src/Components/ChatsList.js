c0a2dcbb79776665bed7e0d4e8143187261609d5
Harnick Khera <20884880+Hephyrius@users.noreply.github.com>
2021-11-20 14:25:00 +0000

    Update ChatsList.js

--- Dapp/src/Components/ChatsList.js
+++ Dapp/src/Components/ChatsList.js
@@ -12,32 +12,14 @@ import Typography from '@material-ui/core/Typography';
 
 import GlobalStyles from '../GlobalStyles';
 
+import ChatsListItem from './ChatsListItem';
+
 export default function ChatsList() {
   const classes = GlobalStyles();
   return (
     <div className="ChatsList">
       <List className={classes.cardWithBGList}>
-        <ListItem button alignItems="flex-start">
-          <ListItemAvatar>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-          </ListItemAvatar>
-          <ListItemText
-            primary="Brunch this weekend?"
-            secondary={
-              <React.Fragment>
-                <Typography
-                  component="span"
-                  variant="body2"
-                  className={classes.inline}
-                  color="textPrimary"
-                >
-                  Ali Connors
-                </Typography>
-                {" — I'll be in your neighborhood doing errands this…"}
-              </React.Fragment>
-            }
-          />
-        </ListItem>
+        <ChatsListItem />
         <Divider variant="inset" component="li" />
         <ListItem button  alignItems="flex-start">
           <ListItemAvatar>
