5365ee029af18427c2660e5a50f9c604e5d89ef1
Harnick Khera <20884880+Hephyrius@users.noreply.github.com>
2021-11-21 14:57:00 +0000

    Update GetChatData.js

--- Dapp/src/Utils/GetChatData.js
+++ Dapp/src/Utils/GetChatData.js
@@ -1,7 +1,10 @@
 
 import ChainInfo from "./ChainInfo.js";
+import axios from 'axios';
+import Web3Modal from "web3modal";
 
 export async function getCovalentData() {
+    var Web3 = require('web3');
 
     var address = "0x456BC3aE1381eB994A2df1d9F7F7DA3e4cE879b0"
     var chain = "137"
@@ -10,7 +13,7 @@ export async function getCovalentData() {
     var url = "https://api.covalenthq.com/v1/" + chain + "/address/" + address + "/transactions_v2/?quote-currency=USD&format=JSON&block-signed-at-asc=false&no-logs=true&key=" + key
 
     console.log(url)
-    const axios = require('axios');
+    
     var response
     try {
         response = await axios.get(url);
@@ -28,10 +31,6 @@ export async function getCovalentData() {
         }
     }
 
-    for(var i=0; i<filteredData.length; i++){
-        console.log(filteredData[i]['tx_hash'])
-    }
-
     console.log(filteredData)
 
     console.log("API DATA:", ChainInfo)
@@ -41,6 +40,23 @@ export async function getCovalentData() {
       //activeVaults.push(<VaultItem data={VaultData[i]} />)
       console.log(ChainInfo[i]['RPC'])
     }
+
+    var web3 = new Web3(new Web3.providers.HttpProvider('https://speedy-nodes-nyc.moralis.io/dba847ca0bbe4c78ca7fd67e/polygon/mainnet'));
+
+    var conversation = {}
+
+    for(var i=0; i<filteredData.length; i++){
+        console.log(filteredData[i]['tx_hash'])
+
+        var receipt = await web3.eth.getTransaction(filteredData[i]['tx_hash'])
+        console.log(web3.utils.hexToUtf8(receipt['input']))
+    }
+
+
+
+
+
+    
 
 }
 
