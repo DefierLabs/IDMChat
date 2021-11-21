65fee60bc22bdd8416649dad80e0b98c6a0b9bbf
Harnick Khera <20884880+Hephyrius@users.noreply.github.com>
2021-11-21 20:28:26 +0000

    add license info

--- Dapp/src/GlobalStyles.js
+++ Dapp/src/GlobalStyles.js
@@ -1,3 +1,21 @@
+/*
+  Copyright 2017 Defier Labs Limited (UK)
+  Licensed under the Business Source License 1.1 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  https://spdx.org/licenses/BUSL-1.1.html
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  Author: Harnick Khera (Github.com/Hephyrius)
+*/
+
+
 import { makeStyles } from '@material-ui/core/styles';
 
 var drawerWidth = 240
