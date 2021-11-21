fa4f4758f3bf6097903edc4d48dfd211b03a91e4
Harnick Khera <20884880+Hephyrius@users.noreply.github.com>
2021-11-21 11:20:17 +0000

    Create ChainInfo.js

---
+++ Dapp/src/Utils/ChainInfo.js
@@ -0,0 +1,45 @@
+export default
+{   "rpcs" : 8,
+    0 :{
+        "chain":"Ethereum",
+        "chainId":"1",
+        "RPC": "https://speedy-nodes-nyc.moralis.io/dba847ca0bbe4c78ca7fd67e/eth/mainnet",
+        "Image": "https://assets.trustwalletapp.com/blockchains/ethereum/info/logo.png"
+    },
+    1 :{
+        "chain":"BSC",
+        "chainId":"56",
+        "RPC": "https://speedy-nodes-nyc.moralis.io/dba847ca0bbe4c78ca7fd67e/bsc/mainnet",
+        "Image": "https://assets.trustwalletapp.com/blockchains/smartchain/info/logo.png"
+    },
+    2 :{
+        "chain":"Polygon",
+        "chainId":"137",
+        "RPC": "https://speedy-nodes-nyc.moralis.io/dba847ca0bbe4c78ca7fd67e/polygon/mainnet",
+        "Image": "https://assets.trustwalletapp.com/blockchains/polygon/info/logo.png"
+    },
+    3 :{
+        "chain":"Arbitrum",
+        "chainId":"1",
+        "RPC": "https://speedy-nodes-nyc.moralis.io/dba847ca0bbe4c78ca7fd67e/arbitrum/mainnet",
+        "Image": "https://assets.trustwalletapp.com/blockchains/arbitrum/info/logo.png"
+    },
+    4 :{
+        "chain":"Avalanche",
+        "chainId":"1",
+        "RPC": "https://speedy-nodes-nyc.moralis.io/dba847ca0bbe4c78ca7fd67e/avalanche/mainnet",
+        "Image": "https://archived.moe/files/biz/thumb/1608/75/1608751041096s.jpg"
+    },
+    5 :{
+        "chain":"Fantom",
+        "chainId":"1",
+        "RPC": "https://speedy-nodes-nyc.moralis.io/dba847ca0bbe4c78ca7fd67e/fantom/mainnet",
+        "Image": "https://assets.trustwalletapp.com/blockchains/smartchain/info/logo.png"
+    },
+    6 :{
+        "chain":"xDai",
+        "chainId":"1",
+        "RPC": "https://xdai.poanetwork.dev",
+        "Image": "https://s2.coinmarketcap.com/static/img/coins/64x64/5601.png"
+    },
+}
