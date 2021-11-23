f7aca55960f7aabcc1eaffa18ddccf1c34611171
Harnick Khera <20884880+Hephyrius@users.noreply.github.com>
2021-11-23 00:54:25 +0000

    rm unused imports

--- Dapp/src/Components/Chat.js
+++ Dapp/src/Components/Chat.js
@@ -23,9 +23,6 @@ import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 
 import ChatMessage from './ChatMessage';
